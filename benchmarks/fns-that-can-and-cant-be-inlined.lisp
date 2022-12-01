ff9d5cb4851b4df4963736fbe4a3dc1a478fb12b
Rob Lewis <rob.y.lewis@gmail.com>
2022-12-01 10:55:04 -0500

    apparently we don't support comments

--- benchmarks/fns-that-can-and-cant-be-inlined.lisp
+++ benchmarks/fns-that-can-and-cant-be-inlined.lisp
@@ -1,14 +1,14 @@
-(define (f x) x) ;; 1 call
-(define (g x y) (+ x y)) ;; 20 calls
-(define (h x y z) (+ (add1 x) (+ (sub1 y) z))) ;; 100 calls
-(define (k x)  ;; large body
+(define (f x) x) 
+(define (g x y) (+ x y)) 
+(define (h x y z) (+ (add1 x) (+ (sub1 y) z))) 
+(define (k x)  
   (do
     true false 1 2 3 4 5 6 7 8 9 0
     (let ((i 1)) i)
     (let ((i true)) i)
     0 9 8 7 6 5 4 3 2 1
     100)) 
-(define (l x) ;; non-leaf function
+(define (l x) 
   (h 1 2 (g 3 (f x))))
 
 (do 
